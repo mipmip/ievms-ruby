eaf975c83504b341290ab6bf58b610fb9d79fa17
Pim Snel <pim@lingewoud.nl>
2015-10-07 09:08:10 +0200

    Gemified step 2

---
+++ lib/ievms/ruby.rb
@@ -0,0 +1,78 @@
+require "ievms/version"
+
+module Ievms
+  class WindowsGuest
+    def initialize(vbox_name)
+      @vbox_name = vbox_name
+
+      is_vm vbox_name
+      wait_for_guestcontrol(vbox_name)
+
+    end
+
+    # Copy a file to the virtual machine from the ievms home folder.
+    def copy_to_vm src ,dest
+      print "Copying #{src} to #{dest}\n"
+      guestcontrol_exec "cmd.exe", "cmd.exe /c copy \"E:\\#{src}\" \"#{dest}\""
+    end
+
+    def upload_file_to_guest(local_path, guest_path)
+
+      # 1 copy to tmp location in .ievms
+      FileUtils.cp local_path, File.join(IEVMS_HOME,File.basename(local_path))
+
+      # 2 run cp command in machine
+      copy_to_vm File.basename(local_path), guest_path
+
+      # 3 remove tmp file in .ievms
+      FileUtils.rm File.join(IEVMS_HOME,File.basename(local_path))
+    end
+
+    def run_bat_as_admin(guest_path)
+      print "Executing as administrator #{guest_path}\n"
+
+      guestcontrol_exec "cmd.exe", "cmd.exe /c \"echo #{guest_path} > C:\\Users\\IEUser\\ievms.bat\""
+      guestcontrol_exec "schtasks.exe", "schtasks.exe /run /tn ievms"
+
+    end
+
+    def run_bat guest_path
+      print "Executing #{guest_path}\n"
+      guestcontrol_exec "cmd.exe", "cmd.exe /c \"#{guest_path}\""
+    end
+
+    private
+
+    # execute final guest control shell cmd
+    def guestcontrol_exec image, cmd
+      cmd = "VBoxManage guestcontrol \"#{@vbox_name}\" run --username \"#{USERNAME}\" --password '#{PASSWD}' --exe \"#{image}\" -- #{cmd}"
+      print cmd + "\n"
+      _, _, _ = Open3.capture3(cmd)
+    end
+
+    # function taken from ievms
+    # Pause execution until guest control is available for a virtual machine
+    def wait_for_guestcontrol vboxname
+      run_level = 0
+      while run_level < 3 do
+        print "Waiting for #{vboxname} to be available for guestcontrol...\n"
+        out = `VBoxManage showvminfo "#{vboxname}" | grep 'Additions run level:'`
+        run_level = out[-2..-2].to_i
+        if run_level < 3
+          sleep 3
+        end
+      end
+    end
+
+    # Is it a virtual machine in VirtualBox?
+    def is_vm vboxname
+      cmd = "VBoxManage showvminfo \"#{vboxname}\""
+
+      _, stderr, _ = Open3.capture3(cmd)
+      if stderr.include? 'Could not find a registered machine named'
+        raise "Virtual Machine #{vboxname} does not exist"
+      end
+    end
+
+  end
+end
---
+++ lib/ievms/version.rb
@@ -0,0 +1,3 @@
+module Ievms
+    VERSION = "0.0.1"
+end
